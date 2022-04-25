2c404e0ce94b92cf8fea8562ad6dd6473c0dbdbf
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-04-25 16:13:49 +0200

    added functionality to create a book and navbar (unstyled)

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -5,9 +5,6 @@ const router = require('express').Router()
 router.get('/books', (req, res, next) => {
     Book.find()
     .then(booksArray => {
-        // console.log(booksArray)
-        console.log(booksArray[0].title)
-        // res.send('great news, your query worked')
         res.render("books/books-list.hbs", {books: booksArray})
     })
     .catch( err => {
@@ -15,5 +12,42 @@ router.get('/books', (req, res, next) => {
         next(err)
     })
 })
+
+router.get("/books/book-create", (req, res, next) => {
+    res.render("books/book-create")
+})
+
+router.post('/books/create', (req, res, next) => {
+    const newBook = {
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+    rating: req.body.rating
+}
+    Book.create(newBook)
+    .then( () =>{
+        res.redirect("/books")
+    })
+    .catch(err => {
+        console.log(err)
+        next(err)
+    })
+})
+
+router.get('/books/:bookId', (req, res, next) => {
+    const id = req.params.bookId
+    console.log(id)
+    Book.findById(id)
+    .then( bookDetails =>{
+        res.render("books/book-details.hbs", {book: bookDetails})
+        console.log(bookDetails)
+    })
+    .catch(err => {
+        console.log(err)
+        next(err)
+    })
+})
+
+
 
 module.exports = router
