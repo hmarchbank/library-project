6c92a5fc544b1623cb87ec1a71e2e6be23e639d2
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-04-26 09:50:14 +0200

    added delete functionality

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -34,6 +34,7 @@ router.post('/books/create', (req, res, next) => {
     })
 })
 
+
 router.get('/books/:bookId', (req, res, next) => {
     const id = req.params.bookId
     console.log(id)
@@ -44,6 +45,51 @@ router.get('/books/:bookId', (req, res, next) => {
     })
     .catch(err => {
         console.log(err)
+        next(err)
+    })
+})
+
+router.get('/books/:bookId/edit', (req, res, next) => {
+    const id = req.params.bookId
+    Book.findById(id)
+    .then(book => {
+        res.render("books/book-edit.hbs", {book})
+    })
+    .catch(err => {
+        console.log(err)
+        next(err)
+    })
+})
+
+router.post('/books/:bookId/edit', (req, res, next) => {
+    const newDetails = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        rating: req.body.rating,
+    }
+
+    const id = req.params.bookId
+    console.log(req.params)
+    Book.findByIdAndUpdate(id, newDetails)
+    .then(response => {
+        res.redirect(`/books/${response._id}`)
+    })
+    .catch(err => {
+        console.log("error updating book", err)
+        next(err)
+    })
+})
+
+router.post("/books/:bookId/delete", (req, res, next) => {
+    console.log(req.params)
+    const id = req.params.bookdId
+    Book.findByIdAndRemove(id)
+    .then( res => {
+        res.redirect('/books')
+    })
+    .catch( err => {
+        console.log("error deleting book",err)
         next(err)
     })
 })
